fd1fbe43d16091cc951169928893dd6189976cb8
sikara <xuhaoxian@gdlinkjob.com>
2024-07-12 17:08:25 +0800

    feat 支持窗口最小化和唤起显示，添加复制事件监听

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,101 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useEffect, useState } from "react";
+
 import "./App.css";
+import { readText, writeText } from "@tauri-apps/api/clipboard";
+
+import { useWindowShowOrHide } from "./hook/key-hook";
+import { UnlistenFn } from "@tauri-apps/api/event";
+import {
+  onTextUpdate,
+  onImageUpdate,
+  onFilesUpdate,
+  startListening,
+  onClipboardUpdate,
+} from "tauri-plugin-clipboard-api";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [lll, setlll] = useState<string[]>([]);
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
+  useWindowShowOrHide();
+  const [copiedText, setCopiedText] = useState("Copied text will be here");
+  let unlistenTextUpdate: UnlistenFn;
+  let unlistenImageUpdate: UnlistenFn;
+  let unlistenClipboard: () => Promise<void>;
+  let unlistenFiles: UnlistenFn;
+
+  useEffect(() => {
+    console.log("on mount");
+
+    const unlistenFunctions = async () => {
+      unlistenTextUpdate = await onTextUpdate((newText) => {
+        console.log(newText);
+        setCopiedText(newText);
+      });
+      unlistenImageUpdate = await onImageUpdate((_) => {
+        console.log("Image updated");
+      });
+      unlistenFiles = await onFilesUpdate((_) => {
+        console.log("Files updated", _);
+      });
+      unlistenClipboard = await startListening();
+
+      onClipboardUpdate(() => {
+        console.log(
+          "plugin:clipboard://clipboard-monitor/update event received"
+        );
+      });
+    };
+
+    unlistenFunctions().catch(console.error);
+
+    return () => {
+      if (unlistenTextUpdate) {
+        unlistenTextUpdate();
+      }
+      if (unlistenImageUpdate) {
+        unlistenImageUpdate();
+      }
+      if (unlistenClipboard) {
+        unlistenClipboard();
+      }
+      if (unlistenFiles) {
+        unlistenFiles();
+      }
+    };
+  }, []);
 
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
+      {lll.map((item, i) => {
+        return <div key={i}>{item}</div>;
+      })}
+      <hr />
+      <button
+        type="button"
+        onClick={async () => {
+          const clipboardText = await readText();
+          clipboardText && setlll([...lll, clipboardText]);
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
+        readText
+      </button>
+      <button
+        type="button"
+        onClick={async () => {
+          await writeText("Tauri is awesome!");
+        }}
+      >
+        writeText
+      </button>
 
-      <p>{greetMsg}</p>
+      <div>
+        <h1>Try and copy this sentence</h1>
+        <h1>{copiedText}</h1>
+      </div>
     </div>
   );
 }
 
 export default App;
+readText;
